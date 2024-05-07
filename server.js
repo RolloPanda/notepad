c981fbd05eab483f7fc9c56a5bb0a58c4f27d904
Ronal <ronalps075@gmail.com>
2024-05-08 01:19:52 +0700

    fix bugs host value

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 (async () => {
   const server = Hapi.server({
     port: 5000,
-    host: 'localhost',
+    host: rocess.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: true,
     },
